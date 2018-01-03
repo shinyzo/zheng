8897c0e646827011527e11afc9c64bdb2534fe50
shinyzo <11742979@qq.com>
2018-01-03 14:33:58 +0800

    修改测试环境参数

--- zheng-cms/zheng-cms-admin/src/main/java/com/zheng/cms/admin/controller/ManageController.java
+++ zheng-cms/zheng-cms-admin/src/main/java/com/zheng/cms/admin/controller/ManageController.java
@@ -1,11 +1,13 @@
 package com.zheng.cms.admin.controller;
 
 import com.zheng.common.base.BaseController;
+import com.zheng.common.util.PropertiesFileUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -26,7 +28,10 @@ public class ManageController extends BaseController {
 	 */
 	@ApiOperation(value = "后台首页")
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
-	public String index() {
+	public String index(ModelMap modelMap) {
+		PropertiesFileUtil propertiesFileUtil = PropertiesFileUtil.getInstance();
+		String upmsSsoServerUrl = propertiesFileUtil.get("zheng.upms.sso.server.url");
+		modelMap.put("upmsSsoServerUrl", upmsSsoServerUrl);
 		return "/manage/index.jsp";
 	}
 
--- zheng-cms/zheng-cms-admin/src/main/webapp/WEB-INF/jsp/manage/index.jsp
+++ zheng-cms/zheng-cms-admin/src/main/webapp/WEB-INF/jsp/manage/index.jsp
@@ -3,5 +3,5 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
 <c:set var="basePath" value="${pageContext.request.contextPath}"/>
-<c:redirect url="http://upms.zhangshuzheng.cn:1111/"/>
+<c:redirect url="${upmsSsoServerUrl}"/>
 <%--<a href='http://upms.zhangshuzheng.cn:1111/sso/logout'>退出登录</a>--%>
